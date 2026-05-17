660a5010175aa038eaadb37a595b7f8563e977ba
Viet Phan Thanh <thanh_viet_phan@hotmail.de>
2026-05-17 20:32:51 +0200

    Create initial NiceGUI app

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from nicegui import ui
+
+
+ui.label("Complaint Management Cockpit").classes("text-h4")
+
+ui.separator()
+
+ui.label("Sprint 1: Grundstruktur läuft mit Python 3.14.")
+
+
+ui.run(title="Complaint Management Cockpit")
